74ee2a411636860bc68f2246de8404f1833c7527
GusTenshou <gustavo.x.amaral7@gmail.com>
2024-10-28 19:18:57 -0300

    light pay route

--- src/app/api/bills/light/route.ts
+++ src/app/api/bills/light/route.ts
@@ -1,11 +1,33 @@
 import { NextRequest, NextResponse } from "next/server";
 import lightningService from "@/services/lightning";
+import { UserService } from "@/services/user";
+
+const userService = new UserService();
 
 export async function POST(req: NextRequest) {
   try {
-    const { amount, memo } = await req.json();
-    const invoice = await lightningService.createInvoice(amount, memo);
+    const { userId, amount, memo } = await req.json();
+
+    // 1. Busca o nodeId do usuário
+    const nodeId = await userService.getNodeIdByUserId(userId);
+
+    if (!nodeId) {
+      return NextResponse.json(
+        { error: "nodeId não encontrado para o usuário." },
+        { status: 404 }
+      );
+    }
+
+    // 2. Gera o invoice usando o nodeId
+    const invoice = await lightningService.createUserInvoice(
+      amount,
+      memo,
+      nodeId
+    );
+
+    // 3. Gera o QR Code do invoice
     const qrCode = await lightningService.generateInvoiceQRCode(invoice);
+
     return NextResponse.json({ qrCode });
   } catch (error) {
     console.error("Erro ao criar invoice no backend:", error);
